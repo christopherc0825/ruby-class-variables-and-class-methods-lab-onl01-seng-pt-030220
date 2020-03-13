1223556a357d6f18b7e9a13ff47906bed4276e2e
Christopher Chaison <cchaison0@gmail.com>
2020-03-13 21:47:28 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,12 +3,14 @@ class Song
   attr_accessor :name, :artist, :genre
   @@count = 0
   @@genres = []
+  @@artists = []
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
     @@count += 1
     @@genres << genre if !@@genres.include?(genre)
+    @@artists << artist if !@@artists.include?(artist)
   end
 
   def count
@@ -17,5 +19,9 @@ class Song
 
   def genres
     @@genres
+  end
+
+  def artists
+    @@artists
   end
 end
