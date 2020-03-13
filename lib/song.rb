01b200ee6e63e37b4c17f7c2e94c41b0bd7f8ab5
Christopher Chaison <cchaison0@gmail.com>
2020-03-13 21:46:09 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,7 +8,7 @@ class Song
     @artist = artist
     @genre = genre
     @@count += 1
-    @@genres << genre if @@genres.!include?(genre)
+    @@genres << genre if !@@genres.include?(genre)
   end
 
   def count
